9f41217aaef036d5de24a395330587bdb3e26a84
Daniel Famiyeh <danielfamiyeh1@gmail.com>
2020-08-31 13:10:43 +0100

    Implements min-max scaling for matrices.

--- src/main/java/data/MinMax.java
+++ src/main/java/data/MinMax.java
@@ -2,6 +2,8 @@ package data;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MinMax {
     public static void scaleVectors(List<List<Double>> vectors){
@@ -40,14 +42,73 @@ public class MinMax {
         }
 
         // Scale feature vectors according to ranges
-        for (int i=0; i<numVectors; i++) {
-            current = vectors.get(i);
-            for (int j=0; j<length; j++) {
-                double range = ranges.get(j);
-                double comp = current.get(j);
-                current.set(j, ((comp - minVals.get(j))/range));
+        for (List<Double> vector : vectors) {
+            current = vector;
+            for (int i = 0; i < length; i++) {
+                double range = ranges.get(i);
+                double comp = current.get(i);
+                current.set(i, ((comp - minVals.get(i)) / range));
             }
         }
 
+    }
+
+    public static void scaleMatrices(List<List<List<Double>>> matrices){
+        int numMatrices = matrices.size();
+        int numRows = matrices.get(0).size();
+        int numCols = matrices.get(0).get(0).size();
+        List<List<Double>> minMatrix = new ArrayList<>();
+        List<List<Double>> maxMatrix = new ArrayList<>();
+        List<List<Double>> ranges = new ArrayList<>();
+        List<List<Double>> currentMatrix;
+        List<Double> currentRow;
+
+        // Get min-max values
+        for(int i=0; i<numMatrices; i++){
+            currentMatrix = matrices.get(i);
+            for(int row=0; row<numRows; row++){
+                currentRow = currentMatrix.get(row);
+                if(i > 0){
+                    for(int col=0; col<numCols; col++) {
+                        double currentMin = minMatrix.get(row).get(col);
+                        double currentMax = maxMatrix.get(row).get(col);
+                        double tentative = currentRow.get(col);
+
+                        if(tentative > currentMax){
+                            maxMatrix.get(row).set(col, tentative);
+                        }
+                        if(tentative < currentMin){
+                            minMatrix.get(row).set(col, tentative);
+                        }
+                    }
+                } else{
+                    minMatrix.add(new ArrayList<>(currentRow));
+                    maxMatrix.add(new ArrayList<>(currentRow));
+                }
+            }
+        }
+
+        // Get ranges
+        for(int row=0; row<numRows; row++){
+            int finalRow = row;
+            ranges.add(IntStream.range(0, numCols)
+            .mapToObj(i ->
+                    maxMatrix.get(finalRow).get(i)
+                    - minMatrix.get(finalRow).get(i))
+            .collect(Collectors.toList()));
+        }
+
+        // Scale matrices
+        for(int i=0; i<numMatrices; i++){
+            int finalI = i;
+            matrices.set(i, IntStream.range(0, numRows).mapToObj(
+                    row -> IntStream.range(0, numCols).mapToDouble(
+                            col -> (matrices.get(finalI).get(row).get(col) -
+                                    minMatrix.get(row).get(col)) / ranges.get(
+                                            row).get(col)
+                            ).boxed().collect(Collectors.toList())
+                    ).collect(Collectors.toList())
+            );
+        }
     }
 }
